054fdaff0ba226d3c7fb498806f129ed780b0d95
Mai Diep <diepmv@vccloud.vn>
2018-06-21 06:18:14 +0700

    working with package

--- hello.go
+++ hello.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
   fmt.Println("Hello, world!")
+
+  otherFunction()
+}
+
+func otherFunction() {
+
 }
---
+++ welcome.go
@@ -0,0 +1,7 @@
+package welcome
+
+const English = "Welcome"
+const Japanese = "safaydfsd"
+
+// Below languages not yet approved by commitee
+const klingon = "Jasfs"
