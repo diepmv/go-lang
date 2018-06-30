2fbfe6e2dc98bb13b0b4a0c85150984b683b3871
Mai Diep <diepmv@vccloud.vn>
2018-06-30 14:54:49 +0700

    import packages

---
+++ temp1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/treehouse-projects/go-intro/welcome"
+)
+
+
+func main() {
+	fmt.Println(welcome.English);
+	fmt.Println(welcome.Japanese);
+	fmt.Println(welcome.klingon);
+}
